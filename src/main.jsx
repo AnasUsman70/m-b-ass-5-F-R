76b1e30f1fe51fccfa50cade970876a58fe97170
AnasUsman70 <159641335+AnasUsman70@users.noreply.github.com>
2024-06-28 20:48:01 +0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<div>
+  <h1>This is first react project</h1>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non mollitia perspiciatis quis.</p>
+  <img src="/vite.svg" alt="" />
+</div>
+)
